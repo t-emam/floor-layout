738354320228bc10743e7c40f7c7401b3666e2d0
Montaserfzy <m.almuhaisen@foodics.com>
2025-02-13 13:48:28 +0300

    adding the draft

---
+++ src/components/_draft.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {inject, computed, definedProps, onMounted} from "vue"
+const __ = inject('--');
+const props = definedProps({})
+const data = computed(()=>{})
+onMounted(()=>{})
+
+</script>
+
+<template>
+   <div class='flex'> </div>
+</template>
